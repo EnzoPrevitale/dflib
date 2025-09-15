8dcef41ce8d1928ebf6a1b611721d139b4a64f3a
EnzoPrevitale <enzogabrielprevitale@gmail.com>
2025-09-15 11:22:42 -0300

    Series

--- src/Main.java
+++ src/Main.java
@@ -1,20 +1,27 @@
 import org.dflib.DataFrame;
+import org.dflib.Extractor;
 import org.dflib.Printers;
 import org.dflib.Series;
+import org.dflib.builder.SeriesAppender;
 
+import java.util.Scanner;
 import java.util.stream.IntStream;
 
-
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
 
-        String[] nomes = {"Enzo", "André", "Antônio", "Alex", "Belai", "Samu", "Pedrão"};
+        SeriesAppender<String, String> appender = Series.byElement(Extractor.<String>$col()).appender();
 
-        Series<String> series = Series.of(nomes);
+        Scanner scanner = new Scanner(System.in);
+        while(scanner.hasNext()) {
+            System.out.println("Digite um texto: ");
+            String text = scanner.next();
+            appender.append(text);
+        }
+        scanner.close();
+
+        Series<String> series = appender.toSeries();
 
         System.out.println(Printers.inline.print(series));
-
     }
 }
